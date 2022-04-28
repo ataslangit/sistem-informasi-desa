8d3c9ce2c90cc95a06338e37fe0f3f6775f6eaf7
opensrcs <98323567+opensrcs@users.noreply.github.com>
2022-04-28 07:15:59 +0700

    Perubahan: upgrade versi CodeIgniter v3

--- donjo-app/config/db_donjo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'ciburial2014';
-$db['default']['database'] = 'sid304ciburial.hol.es';
-?>
