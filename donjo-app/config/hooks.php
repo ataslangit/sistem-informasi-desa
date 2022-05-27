78eecfe4c7fb0911af9b7a864fcb4354ccb3c9e1
satujalur <98323567+satujalur@users.noreply.github.com>
2022-05-27 21:06:03 +0700

    penyesuaian dir config

--- donjo-app/config/hooks.php
+++ donjo-app/config/hooks.php
@@ -1,7 +1,20 @@
-<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
-$hook['pre_system'] = array(
- 'class' => 'Router',
- 'function' => 'route',
- 'filename' => 'router.php',
- 'filepath' => 'hooks'
-);
+<?php
+
+defined('BASEPATH') || exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend CI without hacking the core
+| files.  Please see the user guide for info:
+|
+|	https://codeigniter.com/userguide3/general/hooks.html
+|
+*/
+$hook['pre_system'] = [
+    'class'    => 'Router',
+    'function' => 'route',
+    'filename' => 'router.php',
+    'filepath' => 'hooks',
+];
