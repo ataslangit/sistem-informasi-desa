289b8b3567f0088d76dc4dc553094bbaa33b7837
satujalur <98323567+satujalur@users.noreply.github.com>
2022-06-27 12:14:31 +0700

    perbaikan pada "Tambah Kategori Kelompok Baru"

--- donjo-app/views/kelompok_master/form.php
+++ donjo-app/views/kelompok_master/form.php
@@ -6,35 +6,34 @@
             <h3>Form Kategori Kelompok</h3>
         </div>
         <?= form_open_multipart($form_action, ['id' => 'validasi']) ?>
-            <div class="ui-layout-center" id="maincontent" style="padding: 5px;">
-                <table class="form">
-                    <tr>
-                        <th>Kategori/ Kategori Kelompok</th>
-                        <td><input name="kelompok" type="text" class="inputbox" size="80" value="<?= $kelompok_master['kelompok'] ?>" /></td>
-                    </tr>
-                    <tr>
-                    <tr>
-                        <th width="120" colspan="2">Deskripsi</th>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            <textarea name="deskripsi" style="width:600px; height:300px;resize:none;"><?= $kelompok_master['deskripsi'] ?></textarea>
-                        </td>
-                    </tr>
-                </table>
+        <div class="ui-layout-center" id="maincontent" style="padding: 5px;">
+            <table class="form">
+                <tr>
+                    <th>Kategori/ Kategori Kelompok</th>
+                    <td><input name="kelompok" type="text" class="inputbox" size="80" value="<?= $kelompok_master['kelompok'] ?? '' ?>" /></td>
+                </tr>
+                <tr>
+                <tr>
+                    <th width="120" colspan="2">Deskripsi</th>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <textarea name="deskripsi" style="width:600px; height:300px;resize:none;"><?= $kelompok_master['deskripsi'] ?? '' ?></textarea>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="ui-layout-south panel bottom">
+            <div class="left">
+                <a href="<?= site_url('kelompok_master') ?>" class="uibutton icon prev">Kembali</a>
             </div>
-
-            <div class="ui-layout-south panel bottom">
-                <div class="left">
-                    <a href="<?= site_url() ?>kelompok_master" class="uibutton icon prev">Kembali</a>
-                </div>
-                <div class="right">
-                    <div class="uibutton-group">
-
-                        <button class="uibutton confirm" type="submit">Simpan</button>
-                    </div>
+            <div class="right">
+                <div class="uibutton-group">
+                    <button class="uibutton confirm" type="submit">Simpan</button>
                 </div>
             </div>
+        </div>
         <?= form_close() ?>
     </div>
 </div>
