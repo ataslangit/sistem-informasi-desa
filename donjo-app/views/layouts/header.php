c512eee7c11f29288772a84f63182ef1733f20a0
satujalur <98323567+satujalur@users.noreply.github.com>
2022-06-27 08:46:26 +0700

    update ke html5

--- donjo-app/views/layouts/header.php
+++ donjo-app/views/layouts/header.php
@@ -1,5 +1,5 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 
 <head>
     <title><?php if (@$single_artikel) {
