ccc8edbb8a69f3fa8aec201c1d8d98696997e84e
satujalur <98323567+satujalur@users.noreply.github.com>
2022-08-20 17:03:51 +0700

    tambah migration part 2

---
+++ app/Database/Migrations/2022-08-20-095448_GanalisisParameter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class GanalisisParameter extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'auto_increment' => true,
+            ],
+            'id_indikator' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+            ],
+            'kode_jawaban' => [
+                'type'       => 'INT',
+                'constraint' => 3,
+            ],
+            'asign' => [
+                'type'       => 'TINYINT',
+                'constraint' => '1',
+                'default'    => '0',
+            ],
+            'jawaban' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 200,
+            ],
+            'nilai' => [
+                'type'       => 'TINYINT',
+                'constraint' => 4,
+                'default'    => '0',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('id_indikator');
+        $this->forge->createTable('analisis_parameter', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('analisis_parameter');
+    }
+}
