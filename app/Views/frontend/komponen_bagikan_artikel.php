cf91b61fa895333c9fb56ff63261e131c5861e03
satujalur <98323567+satujalur@users.noreply.github.com>
2022-10-15 21:49:13 +0700

    komentar belum diaktifkan, perlu perbaikan pada fungsi pemanggilannya

--- app/Views/frontend/komponen_bagikan_artikel.php
+++ app/Views/frontend/komponen_bagikan_artikel.php
@@ -10,7 +10,7 @@
         } elseif (isset($artikel->gambar3)) {
             $gambar = $artikel->gambar4;
         } ?>
-        <li class="sbutton" id="FB.Share" name="FB.Share"><a target="_blank" href="https://www.facebook.com/sharer.php?s=100&amp;p[title]=<?= urlencode($artikel->judul); ?>&amp;p[summary]=<?= urlencode($artikel->judul) ?>&amp;p[url]=<?= urlencode(current_url()); ?>&amp;p[images][0]=<?= urlencode(base_url() . '/assets/files/artikel/kecil_' . $gambar); ?>">share on FB</a></li>
+        <li class="sbutton" id="FB.Share" name="FB.Share"><a target="_blank" href="https://www.facebook.com/sharer.php?s=100&amp;p[title]=<?= urlencode($artikel->judul) ?>&amp;p[summary]=<?= urlencode($artikel->judul) ?>&amp;p[url]=<?= urlencode(current_url()); ?>&amp;p[images][0]=<?= urlencode(base_url() . '/assets/files/artikel/kecil_' . $gambar) ?>">share on FB</a></li>
         <li class="sbutton" id="rt"><a target="_blank" href="http://twitter.com/share" class="twitter-share-button">Tweet</a></li>
     </ul>
 </div>
