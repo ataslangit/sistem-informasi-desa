7021f08720bd19012c7d67e9ddfc8cd3bdc63504
satujalur <98323567+satujalur@users.noreply.github.com>
2022-10-16 22:15:05 +0700

    (#111) ganti donjo-sys dengan CI2.1.0

--- donjo-sys/core/CodeIgniter.php
+++ donjo-sys/core/CodeIgniter.php
@@ -33,7 +33,7 @@
  * @var string
  *
  */
-	define('CI_VERSION', '2.1.4');
+	define('CI_VERSION', '2.1.0');
 
 /**
  * CodeIgniter Branch (Core = TRUE, Reactor = FALSE)
--- donjo-sys/language/english/db_lang.php
+++ donjo-sys/language/english/db_lang.php
@@ -1,8 +1,8 @@
 <?php
 
 $lang['db_invalid_connection_str'] = 'Unable to determine the database settings based on the connection string you submitted.';
-$lang['db_unable_to_connect'] = 'Ada kesalahan dalam pemasangan SID. Aplikasi tidak bisa terhubung ke database. Silakan periksa pada berkas di donjo-app/config/database.php. Pastikan pada bagian "Data Konfigurasi MySQL yang disesuaikan" terisi dengan benar.';
-$lang['db_unable_to_select'] = 'Unable to Tabel';
+$lang['db_unable_to_connect'] = 'Unable to connect to your database server using the provided settings.';
+$lang['db_unable_to_select'] = 'Unable to select the specified database: %s';
 $lang['db_unable_to_create'] = 'Unable to create the specified database: %s';
 $lang['db_invalid_query'] = 'The query you submitted is not valid.';
 $lang['db_must_set_table'] = 'You must set the database table to be used with your query.';
@@ -23,7 +23,7 @@ $lang['db_table_name_required'] = 'A table name is required for that operation.'
 $lang['db_column_name_required'] = 'A column name is required for that operation.';
 $lang['db_column_definition_required'] = 'A column definition is required for that operation.';
 $lang['db_unable_to_set_charset'] = 'Unable to set client connection character set: %s';
-$lang['db_error_heading'] = 'Terjadi Kesalahan Database';
+$lang['db_error_heading'] = 'A Database Error Occurred';
 
 /* End of file db_lang.php */
 /* Location: ./system/language/english/db_lang.php */
