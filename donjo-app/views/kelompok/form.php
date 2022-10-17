78d94574c96fc7366421fccf2ff91b5e9047cf40
satujalur <98323567+satujalur@users.noreply.github.com>
2022-10-17 21:07:54 +0700

    fix

--- donjo-app/views/kelompok/form.php
+++ donjo-app/views/kelompok/form.php
@@ -39,11 +39,11 @@
                             <table class="form">
                                 <tr>
                                     <th>Nama kelompok</th>
-                                    <td><input name="nama" type="text" class="inputbox" size="80" value="<?php echo $kelompok['nama'] ?>"></td>
+                                    <td><input name="nama" type="text" class="inputbox" size="80" value="<?php echo $kelompok['nama'] ?? '' ?>"></td>
                                 </tr>
                                 <tr>
                                     <th>Nomor / Kode kelompok</th>
-                                    <td><input name="kode" type="text" class="inputbox" size="80" value="<?php echo $kelompok['kode'] ?>"></td>
+                                    <td><input name="kode" type="text" class="inputbox" size="80" value="<?php echo $kelompok['kode'] ?? '' ?>"></td>
                                 </tr>
                                 <tr>
                                     <th width="100">Kategori Kelompok</th>
@@ -51,7 +51,7 @@
                                         <select name="id_master" onchange="formAction('mainform','<?php echo site_url('kelompok/filter') ?>')" class="required">
                                             <option value="">-- Kategori Kelompok --</option>
                                             <?php foreach ($list_master as $data) { ?>
-                                                <option value="<?php echo $data['id'] ?>" <?php if ($kelompok['id_master'] == $data['id']) : ?>selected<?php endif ?>><?php echo $data['kelompok'] ?></option>
+                                                <option value="<?php echo $data['id'] ?>" <?php if ($kelompok !== null && $kelompok['id_master'] == $data['id']) : ?>selected<?php endif ?>><?php echo $data['kelompok'] ?></option>
                                             <?php } ?>
                                         </select>
                                     </td>
@@ -62,29 +62,29 @@
                                         <div id="id_ketua" name="id_ketua"></div>
                                     </td>
                                 </tr>
-                                <th colspan="2">Deskripsi kelompok</th>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <textarea name="keterangan" style="width: 600px; height: 300px;resize:none;"><?php echo $kelompok['keterangan'] ?></textarea>
+                                <tr>
+                                    <th colspan="2">Deskripsi kelompok</th>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <textarea name="keterangan" style="width: 600px; height: 300px;resize:none;"><?php echo $kelompok['keterangan'] ?? '' ?></textarea>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="ui-layout-south panel bottom">
+                            <div class="left">
+                                <a href="<?php echo site_url('kelompok') ?>" class="uibutton icon prev">Kembali</a>
+                            </div>
+                            <div class="right">
+                                <div class="uibutton-group">
+                                    <button class="uibutton confirm" type="submit">Simpan</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </td>
         </tr>
     </table>
-</div>
-<div class="ui-layout-south panel bottom">
-    <div class="left">
-        <a href="<?php echo site_url() ?>kelompok" class="uibutton icon prev">Kembali</a>
-    </div>
-    <div class="right">
-        <div class="uibutton-group">
-
-            <button class="uibutton confirm" type="submit">Simpan</button>
-        </div>
-    </div>
-</div>
-</form>
-</div>
-</td>
-</tr>
-</table>
 </div>
