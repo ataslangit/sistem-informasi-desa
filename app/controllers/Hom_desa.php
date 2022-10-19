dc0126599e9d76a148e7397b4303990a8e3e6e60
satujalur <98323567+satujalur@users.noreply.github.com>
2022-10-19 20:44:03 +0700

    (#141) Upgrade CodeIgniter3

---
+++ app/controllers/Hom_desa.php
@@ -0,0 +1,80 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Hom_Desa extends CI_Controller{
+
+	function __construct(){
+		parent::__construct();
+
+		$this->load->model('user_model');
+		$grup	= $this->user_model->sesi_grup($_SESSION['sesi']);
+		if($grup!=1 AND $grup!=2) redirect('siteman');
+		$this->load->model('header_model');
+		$this->load->model('config_model');
+	}
+
+	function index(){
+		$_SESSION['delik'] = 0;
+		$nav['act']= 0;
+		$header = $this->header_model->get_data();
+		$data['main'] = $this->config_model->get_data();
+		$this->load->view('header',$header);
+		$this->load->view('home/nav',$nav);
+		$this->load->view('home/konfigurasi_form',$data);
+		$this->load->view('footer');
+	}
+
+	function about(){
+		$nav['act']= 2;
+		$header = $this->header_model->get_data();
+		$this->load->view('header',$header);
+		$this->load->view('home/nav',$nav);
+		$this->load->view('home/desa');
+		$this->load->view('footer');
+	}
+
+	function insert(){
+		$this->config_model->insert();
+		redirect('hom_desa');
+	}
+
+	function update($id=''){
+		$this->config_model->update($id);
+		redirect("hom_desa");
+	}
+
+	function ajax_kantor_maps(){
+		$data['desa'] = $this->config_model->get_data();
+		$data['form_action'] = site_url("hom_desa/update_kantor_maps/");
+		$this->load->view("home/ajax_kantor_desa_maps", $data);
+	}
+
+	function ajax_wilayah_maps(){
+		$data['desa'] = $this->config_model->get_data();
+		$data['form_action'] = site_url("hom_desa/update_wilayah_maps/");
+		$this->load->view("home/ajax_wilayah_desa_maps", $data);
+	}
+
+	function update_kantor_maps(){
+		$this->config_model->update_kantor();
+		redirect("hom_desa");
+	}
+
+	function update_wilayah_maps(){
+		$this->config_model->update_wilayah();
+		redirect("hom_desa");
+	}
+
+	function kosong_pend(){
+		$this->config_model->kosong_pend();
+		redirect("hom_desa");
+	}
+
+	function undelik(){
+		if(isset($_SESSION['delik'])){
+			unset($_SESSION['delik']);
+		}
+		redirect("analisis_master/clear");
+	}
+
+}
---
+++ app/controllers/Siteman.php
@@ -0,0 +1,36 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+class Siteman extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+
+		$this->load->model('header_model');
+		$this->load->model('user_model');
+		$this->load->model('config_model');
+	}
+	function index(){
+		$this->user_model->logout();
+		$header = $this->header_model->get_config();
+
+
+		if(!isset($_SESSION['siteman']))
+		$_SESSION['siteman']=0;
+		$_SESSION['success']  = 0;
+		$_SESSION['per_page'] = 10;
+		$_SESSION['cari']  = '';
+		$_SESSION['pengumuman'] = 0;
+		$_SESSION['sesi'] = "kosong";
+		$_SESSION['timeout'] = 0;
+
+		$this->load->view('siteman',$header);
+		$_SESSION['siteman']=0;
+	}
+	function auth(){
+		$this->config_model->do_reg();
+		$this->user_model->siteman();
+		redirect('main');
+	}
+	function login(){
+		$this->user_model->logout();
+		redirect('siteman');
+	}
+}
