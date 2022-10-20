577c9ade4fe3c07ae578a0abd6d7a8a32ee5ccc8
Toto <mail@totoprayogo.com>
2022-10-20 15:34:09 +0700

    (#146) Pembaruan constant `VERSI_SID`

--- app/config/constants.php
+++ app/config/constants.php
@@ -84,4 +84,4 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-define('VERSI_SID', '3.11.0');
+defined('VERSI_SID') OR define('VERSI_SID', '3.11.0');
