96e7c62512f58f8f836241bfabf406864a7193df
totoprayogo1916 <totoprayogo1916@gmail.com>
2022-10-25 20:57:54 +0700

    tambahkan php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use CodeIgniter\CodingStandard\CodeIgniter4;
+use Nexus\CsConfig\Factory;
+use Nexus\CsConfig\FixerGenerator;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__)
+    ->exclude('donjo-sys')
+    ->exclude('donjo-app/views');
+
+$overrides = [];
+
+$options = [
+    'finder'       => $finder,
+    'customFixers' => FixerGenerator::create('donjo-sys/nexusphp/cs-config/src/Fixer', 'Nexus\\CsConfig\\Fixer'),
+];
+
+return Factory::create(new CodeIgniter4(), $overrides, $options)->forProjects();
