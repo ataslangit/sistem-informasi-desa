a81891b725d30a07a9944c9c0c54b3b12ef18961
totoprayogo1916 <totoprayogo1916@gmail.com>
2023-05-18 19:40:21 +0700

    upgrade ci4.3.4

--- donjo-app/Config/App.php
+++ donjo-app/Config/App.php
@@ -105,8 +105,10 @@ class App extends BaseConfig
      *
      * The default timezone that will be used in your application to display
      * dates with the date helper, and can be retrieved through app_timezone()
+     *
+     * @see https://www.php.net/manual/en/timezones.php for list of timezones supported by PHP.
      */
-    public string $appTimezone = 'UTC';
+    public string $appTimezone = 'Asia/Jakarta';
 
     /**
      * --------------------------------------------------------------------------
