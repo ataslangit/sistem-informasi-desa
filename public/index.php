6627b483520a197e6233e038e09e7852b7f93b05
totoprayogo1916 <totoprayogo1916@gmail.com>
2023-08-27 12:06:39 +0700

    fix path

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
+require FCPATH . '../donjo-app/Config/Paths.php';
 // ^^^ Change this line if you move your application folder
 
 $paths = new Config\Paths();
