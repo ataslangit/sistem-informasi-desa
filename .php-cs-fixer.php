faf6a894e063aba6c467d835139ebf860d21f6e6
Toto <totoprayogo1916@gmail.com>
2023-10-06 00:12:57 +0700

    (#486) update php-cs-fixer actions

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,14 +7,21 @@ use Nexus\CsConfig\Factory;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
+    ->files()
     ->in(__DIR__)
-    ->exclude('donjo-sys')
-    ->exclude('donjo-app/Views');
+    ->exclude(['donjo-app/Views', 'donjo-sys', 'node_modules'])
+    ->append([
+        __FILE__,
+        __DIR__ . '/rector.php',
+    ]);
 
-$overrides = [];
+$overrides = [
+    // 'declare_strict_types' => true,
+    // 'void_return'          => true,
+];
 
 $options = [
-    'finder'       => $finder,
+    'finder' => $finder,
 ];
 
 return Factory::create(new CodeIgniter4(), $overrides, $options)->forProjects();
