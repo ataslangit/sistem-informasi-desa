35dca2771d41e5c7ab79ecbc162afe9a0d8d0689
totoprayogo1916 <totoprayogo1916@gmail.com>
2023-10-12 15:30:43 +0700

    kembalikan

--- donjo-app/Config/Autoload.php
+++ donjo-app/Config/Autoload.php
@@ -99,7 +99,7 @@ class Autoload extends AutoloadConfig
      * @phpstan-var list<string>
      */
     public $helpers = [
-        // 'donjolib',
+        'donjolib',
         'html',
         'pict',
         'text',
