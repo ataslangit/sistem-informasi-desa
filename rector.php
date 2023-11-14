1302b5c0941fecac6ebf366520513dbcbb39d1fa
totoprayogo1916 <2387514+totoprayogo1916@users.noreply.github.com>
2023-11-14 15:49:09 +0700

    up

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnExprInConstructRector;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
@@ -29,6 +30,7 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/donjo-app/third_party/',
         __DIR__ . '/donjo-app/Views/',
         JsonThrowOnErrorRector::class,
+        RemoveUselessReturnExprInConstructRector::class,
     ]);
 
     // Include Composer's autoload - required for global execution, remove if running locally
