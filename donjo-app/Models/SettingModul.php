79cd6b522388b401a32cb9f9b35c8692c537cfcf
totoprayogo1916 <2387514+totoprayogo1916@users.noreply.github.com>
2023-11-20 22:46:15 +0700

    sidebar

---
+++ donjo-app/Models/SettingModul.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SettingModul extends Model
+{
+    protected $table            = 'setting_modul';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'id',
+        'modul',
+        'url',
+        'aktif',
+        'ikon',
+        'urut',
+        'level',
+        'hidden',
+    ];
+
+}
