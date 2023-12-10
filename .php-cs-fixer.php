d83e432384812948cc725967fba44aa3e0a2672f
totoprayogo1916 <2387514+totoprayogo1916@users.noreply.github.com>
2023-12-11 00:56:15 +0700

    (#181) Rapikan kode dengan bantuan php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,18 +4,17 @@ declare(strict_types=1);
 
 use CodeIgniter\CodingStandard\CodeIgniter4;
 use Nexus\CsConfig\Factory;
-use Nexus\CsConfig\FixerGenerator;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
     ->in(__DIR__)
-    ->exclude(['donjo-app/Views', 'donjo-sys']);
+    ->exclude('donjo-sys')
+    ->exclude('donjo-app/Views');
 
 $overrides = [];
 
 $options = [
     'finder'       => $finder,
-    // 'customFixers' => FixerGenerator::create('donjo-sys/nexusphp/cs-config/src/Fixer', 'Nexus\\CsConfig\\Fixer'),
 ];
 
 return Factory::create(new CodeIgniter4(), $overrides, $options)->forProjects();
