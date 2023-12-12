8edf06367aa2777aa38723a4b5433f9558a1ff5e
totoprayogo1916 <2387514+totoprayogo1916@users.noreply.github.com>
2023-12-12 22:52:52 +0700

    update footer

--- donjo-app/Views/siteman/partials/footer.php
+++ donjo-app/Views/siteman/partials/footer.php
@@ -3,10 +3,12 @@
     <div class="content py-3">
         <div class="row fs-sm">
             <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
-                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
+                Sistem Informasi Desa v<?= VERSI_SID ?><br>
+                Lihat pengembangan kode di <a href="https://github.com/ataslangit/sistem-informasi-desa" target="_blank">GitHub</a>
             </div>
             <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
-                <a class="fw-semibold" href="https://1.envato.market/95j" target="_blank">Codebase 5.1</a> &copy; <span data-toggle="year-copy"></span>
+                &copy; 2009 - 2016 Combine Resource Institution<br>
+                &copy; 2022 - 2023 Atas Langit
             </div>
         </div>
     </div>
