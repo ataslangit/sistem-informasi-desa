0b931e529197a530da8f851c73003a92587b8eee
Toto <totoprayogo1916@gmail.com>
2025-10-12 22:07:53 +0700

    (#601) refactor(routing): tingkatkan keamanan \& prediktabilitas rute

--- donjo-app/models/Analisis_respon_model.php
+++ donjo-app/models/Analisis_respon_model.php
@@ -782,8 +782,8 @@ class Analisis_respon_model extends CI_Model
                 $par             = $query2->result_array();
                 $data[$i]['par'] = $par;
 
-            //	$j++;
-            // }
+                //	$j++;
+                // }
             } else {
                 $data[$i]['par'] = null;
             }
@@ -1049,7 +1049,7 @@ class Analisis_respon_model extends CI_Model
                                     $n++;
                                 }
                             }
-                        // }
+                            // }
                         } else {
                             $sql   = 'SELECT id FROM analisis_parameter WHERE id_indikator = ? AND jawaban = ?;';
                             $query = $this->db->query($sql, [$indi['id'], $isi]);
@@ -1187,7 +1187,7 @@ class Analisis_respon_model extends CI_Model
                                 $n++;
                             }
                         }
-                    // }
+                        // }
                     } else {
                         $sql   = 'SELECT id FROM analisis_parameter WHERE id_indikator = ? AND jawaban = ?;';
                         $query = $this->db->query($sql, [$indi['id'], $isi]);
@@ -1319,7 +1319,7 @@ class Analisis_respon_model extends CI_Model
                                 $n++;
                             }
                         }
-                    // }
+                        // }
                     } else {
                         $sql   = 'SELECT id FROM analisis_parameter WHERE id_indikator = ? AND jawaban = ?;';
                         $query = $this->db->query($sql, [$indi['id'], $isi]);
