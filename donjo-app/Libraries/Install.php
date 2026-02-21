63ac31f19cc2b37e16464c3fae5caddeb162f770
Toto <totoprayogo1916@gmail.com>
2026-02-21 12:07:58 +0700

    (#628) fix: perbaiki kompatibilitas codeigniter 3

--- donjo-app/Libraries/Install.php
+++ donjo-app/Libraries/Install.php
@@ -21,7 +21,7 @@ class Install
         $db    = $this->CI->db->dbprefix . $this->CI->db->database;
         $sql   = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA=? AND TABLE_NAME <> 'impor'";
         $query = $this->CI->db->query($sql, $db);
-        $data  = $query->getResultArray();
+        $data  = $query->result_array();
 
         return ! (count($data) !== 77);
     }
